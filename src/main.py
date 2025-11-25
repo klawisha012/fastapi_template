6fb711bbab6268e6ad37a720356adca5814b49f4
zwarder <klawisha012@gmail.com>
2025-11-25 04:26:38 +0300

    delete apps before db testing

--- src/main.py
+++ src/main.py
@@ -3,15 +3,12 @@
 
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
-from src.firstapp.router import router
 import uvicorn
 from src.config import database_settings
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
-
-app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", port=8000, reload=True)
